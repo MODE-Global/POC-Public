9ee09473e02ce50a16d57e4abca84184e7d1eccf
ulageswaris <114491810+ulageswaris@users.noreply.github.com>
2022-11-23 10:46:15 -0500

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,31 +1,32 @@
-name: 'Microsoft Teams Webhook Message Bot'
-description: 'GitHub Action to send a message to Microsoft Teams using a webhook - Included an advanced options for a nice notifications'
-author: 'myusufcse'
-branding:
-  icon: 'message-square'
-  color: 'blue'
-inputs:
-  webhook:
-    description: 'The webhook url for your Incoming Webhook connector'
-    required: true
-  message:
-    description: 'The message you want to send to your Microsoft Teams channel'
-    required: true
-  notification_color:
-    description: 'The notification color for your message either success, danger, warning or info'
-    required: false
-  button_link_text:
-    description: 'The button text for navigation to link from Microsoft Teams channel'
-    required: false
-  button_link:
-    description: 'The button link for navigation from Microsoft Teams channel'
-    required: false
-runs:
-  using: 'docker'
-  image: 'Dockerfile'
-  args:
-    - ${{ inputs.webhook }}
-    - ${{ inputs.message }}
-    - ${{ inputs.notification_color }}
-    - ${{ inputs.button_link_text }}
-    - ${{ inputs.button_link }}
+import pymsteams
+import sys
+
+color_scheme={'success':'00c551','danger':'ff4444','warning':'ffbb33','info':'33b5e5'}
+
+print(len(sys.argv))
+for i in range(1, len(sys.argv)):
+    print(sys.argv[i])
+
+if len(sys.argv) < 2:
+  raise Exception("Please pass the webhooks and message for the notification")
+
+webhook = sys.argv[1]
+message = sys.argv[2]
+
+myTeamsMessage = pymsteams.connectorcard(webhook)
+
+myTeamsMessage.text(message)
+
+if len(sys.argv) > 3:
+    notification_color = color_scheme.get(sys.argv[3], '33b5e5')
+    myTeamsMessage.color(notification_color)
+else:
+    notification_color = color_scheme.get('info', '33b5e5')
+    myTeamsMessage.color(notification_color)
+
+if len(sys.argv) > 4:
+    button_link_text = sys.argv[4]
+    button_link = sys.argv[5]
+    myTeamsMessage.addLinkButton(button_link_text, button_link)
+
+myTeamsMessage.send()
