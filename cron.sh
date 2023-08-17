de72e1eac46def1f4c9a298bf263f27a45cb55a7
ulageswaris <114491810+ulageswaris@users.noreply.github.com>
2023-08-17 18:40:59 -0400

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# Check if the current date's month and day match August 18th (08-18)
 if [[ "$(date '+%m-%d')" == "08-18" ]]; then
     echo "Triggering the Scheduled Pipeline"
 else
     echo "Skipping the Pipeline as it is not scheduled to run on this specified date"
-    exit 1
+    exit 1  # Exit with an error status code
 fi
