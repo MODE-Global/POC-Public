3193491b710ad78fef92b0548e5a094d53e621f3
ulageswaris <114491810+ulageswaris@users.noreply.github.com>
2023-08-17 18:47:13 -0400

    Delete cron.sh

--- cron.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check if the current date's month and day match August 18th (08-18)
-if [[ "$(date '+%m-%d')" == "08-18" ]]; then
-    echo "Triggering the Scheduled Pipeline"
-else
-    echo "Skipping the Pipeline as it is not scheduled to run on this specified date"
-    exit 1  # Exit with an error status code
-fi
