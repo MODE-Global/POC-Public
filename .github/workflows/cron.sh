be7560f61adc35889309f9c0dd2642b505bae59b
ulageswaris <114491810+ulageswaris@users.noreply.github.com>
2023-08-17 18:26:07 -0400

    Delete cron.sh

--- .github/workflows/cron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ "$(date '+%m-%d')" == "08-18" ]]; then
-    echo "Triggering the Scheduled Pipeline"
-else
-    echo "Skipping the Pipeline as it is not scheduled to run on this specified date"
-    exit 1
-fi
